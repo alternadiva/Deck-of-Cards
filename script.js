149aeb77fd1aaf88fd2cd8e1372568349544e07b
alternadiva <asomeonelse@gmail.com>
2022-01-26 20:47:01 +0000

    call functions, add event listener

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 let suits = ["spades", "diamonds", "clubs", "hearts"];
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
-let deckDiv = document.getElementById("deckk");
+let deck = [];
+let deckDiv = document.getElementById("deck");
 
 function getDeck() {
     let deck = [];
     for (let i = 0; i < suits.length; i++) {
         for (let j = 0; j < values.length; j++) {
-            let card = {suit: suits[i], value: values[j]};
+            let card = {value: values[j], suit: suits[i]};
             deck.push(card);
         }
     }
     return deck;
 }
 
+let fragment = document.createDocumentFragment();
+
 function shuffle() {
-    for (let i = 0; i < 100; i++) {
-        let location1 = Math.floor(Math.random() * deck.length);
-        deck = location1;
+    for (let i = 0; i < 1000; i++) {
+		let location1 = Math.floor((Math.random() * deck.length));
+		let location2 = Math.floor((Math.random() * deck.length));
+		let temporaryCard = deck[location1];
+
+		deck[location1] = deck[location2]; 
+		deck[location2] = temporaryCard; 
+  
     }
     renderDeck();
 }
@@ -30,19 +37,19 @@ function renderDeck() {
         let icon = "";
         
         if (deck[i].suit == "spades") {
-            icon = "&spades";
+            icon = "&spades;";
         }
         else if (deck[i].suit == "diamons") {
-            icon = "&diams";
+            icon = "&diams;";
         }
         else if (deck[i].suit == "clubs") {
-            icon = "&clubs";
+            icon = "&clubs;";
         }
         else {
-            icon = "&hearts";
+            icon = "&hearts;";
         }
 
-        card.innerHTML = deck[i].value + "" + icon;
+        card.innerHTML = (deck[i].value + "" + icon) + (deck[i].value + "" + icon);
         card.classList.add("card");
         card.classList.add(deck[i].suit);
 
@@ -50,3 +57,10 @@ function renderDeck() {
     }
 }
 
+function start() {
+    deck = getDeck();
+    shuffle();
+    renderDeck();
+}
+
+window.addEventListener('load', start);
